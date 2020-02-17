2903c6c75d2b03bf854b2bd377418fdaa09b145f
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-02-18 00:09:59 +0900

    Update baekjoon_1389_케빈베이컨_notyet.py

--- baekjoon_1389_케빈베이컨_notyet.py
+++ baekjoon_1389_케빈베이컨_notyet.py
@@ -4,39 +4,37 @@ def kevin(start,end,fre_len=0):
     
     if start == end and su_len>fre_len:
         su_len = fre_len
-        return 
+        
 
-    for z in range(N):
-        if visit[z] == 0:
+    for z in range(1,N+1):
+        if visit[z] == 0 and friend[start][z] == 1:
             visit[z] = 1
-            kevin(z,end,fre_len+1)
+            kevin(z,end,fre_len+1) == 1
             visit[z] = 0
 
 N,M = map(int,input().split())
 
-friend = [[0]*N for _ in range(N)]
+friend = [[0]*(N+1) for _ in range(N+1)]
 
 for _ in range(M):
     fre1, fre2 = map(int,input().split())
-    friend[fre1-1][fre2-1] = 1
-    friend[fre2-1][fre1-1] = 1
+    friend[fre1][fre2] = 1
+    friend[fre2][fre1] = 1
 
 min_len = N*M
-for i in range(N):
+V=N+5
+for i in range(1,N+1):
     su = 0
-    for j in range(N):
+    for j in range(1,N+1):
         if i==j:
             continue
-        visit=[0]*N
+        visit=[0]*(N+1)
         su_len = N*M
         kevin(i,j)
         su += su_len
-    if min_len>su:
-        min_len=su
+    if min_len > su:
+        min_len = su
         V=i
-    elif min_len == su:
-        if V>i:
-            V=i
-    print(su,i)
-    print()
+    elif min_len == su and V>i:
+        V=i
 print(V)
