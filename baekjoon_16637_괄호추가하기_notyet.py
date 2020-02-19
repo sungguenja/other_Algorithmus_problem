fd55d8520c1266cebed04f4846bcd0eba9cf1a8c
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-02-19 23:50:24 +0900

    0219 dont' be lazy

--- baekjoon_16637_괄호추가하기_notyet.py
+++ baekjoon_16637_괄호추가하기_notyet.py
@@ -1,26 +1,32 @@
-from itertools import combinations
-import copy
+# def bracket_exp(N,exp,num=[]):
+#     if visit == [1]*(N//2):
+#         root.append(num)
+#         return
 
-N=int(input())
-exp = list(input())
-new_exp = []
-for i in range(0,N-1,2):
-    new_exp.append(exp[i:i+3])
+#     for i in range(N//2):
+#         if visit[i] == 0:
+#             visit[i] = 1
+#             bracket_exp(N,exp,num+[i])
+#             visit[i] = 0
 
-# 초기값 잡기
-if new_exp[0][1] == '+':
-    su = int(new_exp[0][0])+int(new_exp[0][2])
-elif new_exp[0][1] == '-':
-    su = int(new_exp[0][0])-int(new_exp[0][2])
-elif new_exp[0][1] == '*':
-    su = int(new_exp[0][0])*int(new_exp[0][2])
-# 초기값 잡기 마무리
-for i in range(1,N//2):
-    if new_exp[i][1] == '+':
-        su += int(new_exp[i][2])
-    elif new_exp[i][1] == '-':
-        su -= int(new_exp[i][2])
-    elif new_exp[i][1] == '*':
-        su *= int(new_exp[i][2])
-print(su)
+# 생각 잘못했다 시부장
 
+# N=int(input())
+# exp = list(input())
+# copy_exp = exp[:]
+# visit = [0]*(N//2)
+# root=[]
+# bracket_exp(N,exp)
+# su = -10*(N//2+1)
+# for i in range(len(root)):
+#     exp = copy_exp[:]
+#     for j in range(N//2):
+#         if exp[2*(root[i][j])+1] == '+':
+#             exp[2*(root[i][j]):2*(root[i][j])+3] = [int(exp[2*(root[i][j])])+int(exp[2*(root[i][j])+2])]*3
+#         elif exp[2*(root[i][j])+1] == '-':
+#             exp[2*(root[i][j]):2*(root[i][j])+3] = [int(exp[2*(root[i][j])])-int(exp[2*(root[i][j])+2])]*3
+#         elif exp[2*(root[i][j])+1] == '*':
+#             exp[2*(root[i][j]):2*(root[i][j])+3] = [int(exp[2*(root[i][j])])*int(exp[2*(root[i][j])+2])]*3
+#     if su<exp[2*(root[i][j])+1]:
+#         su = exp[2*(root[i][j])+1]
+# print(su)
