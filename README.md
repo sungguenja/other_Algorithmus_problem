713b56439defd4fceb22ee236d047071c2bf7ca6
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-03-08 22:46:20 +0900

    0308
    I study with book and I only read that I will solve problem nextday

--- README.md
+++ README.md
@@ -11,3 +11,28 @@ but this repository doesn't have euler's problem
 I have some repositories and one of them is for euler
 
 If you have interest for that pls visit that repository and pls say something for my study lol
+
+
+
+I will remind this rule
+
+pls I wish I can :pray:
+
+> 1. think intuition and systemic contact
+>    1.  think this problem is like that I solved
+>    2.  It can be solved simple way
+> 2. I can express my think to formulation
+> 3. It can be leveling
+> 4. can it be painted
+> 5. can't solve problem think reverse
+
+# we can solve all problem and it can't be now
+
+# maybe we need to see answer
+
+# but we must solve problem someday
+
+# we can do it
+
+
+
--- baekjoon_16236_아기상어.py
+++ baekjoon_16236_아기상어.py
@@ -1,4 +1,4 @@
-direction = [[-1,0],[0,-1],[1,0],[0,1]]
+direction = [[-1,0],[0,-1],[0,1],[1,0]]
 
 N=int(input())
 aqua = []
@@ -7,31 +7,56 @@ for i in range(N):
     horizon=[]
     horizon=list(map(int,input().split()))
     for j in range(N):
-        if 0<horizon[j]<9:
-            fish.append([i,j])
-        elif horizon[j]==9:
+        if horizon[j]==9:
             shark = [i,j]
+            break
     aqua.append(horizon)
 
 t=0
 eat=0
 big=2
-que=[[shark[0],shark[1],0,9]]
+que=[[shark[0],shark[1],0,0,5]]
 visit =[[0]*N for _ in range(N)]
 aqua[que[0][0]][que[0][1]]=0
+visit[que[0][0]][que[0][1]]=1
 d=0
 while que != []:
     X=que.pop(0)
-    i,j,d,size=X[0],X[1],X[2],X[3]
-    for k in direction:
-        ni=i+k[0]
-        nj=j+k[1]
-        if 0<=ni<N and 0<=nj<N and visit[ni][nj]==0:
-            if aqua[ni][nj] <= big or aqua[ni][nj] == 9:
-                if aqua[ni][nj] == 0 or aqua[ni][nj] == big:
-                    visit[ni][nj]=1
-                    que.append([ni,nj,d+1,0])
-                elif 0<aqua[ni][nj]<big:
-                    visit[ni][nj]=1
-                    que.append([ni,nj,d+1,aqua[ni][nj]])
+    i,j,d,size,goal=X[0],X[1],X[2],X[3],X[4]
+    trigger = False
+    if size!=0:
+        que = [[i,j,0,0,5]]
+        visit =[[0]*N for _ in range(N)]
+        aqua[i][j] = 0
+        visit[i][j] = 1
+        t+=d
+        eat+=1
+        if eat == big:
+            big+=1
+            eat=0
+    else:
+        for k in range(4):
+            ni=i+direction[k][0]
+            nj=j+direction[k][1]
+            if goal == 1 and k == 3:
+                for point in range(len(que)):
+                    if nj+1<N and que[point][0] == ni and que[point][1] == nj+1:
+                        trigger = True
+                        break
+            elif goal == 2 and k == 3:
+                for point in range(len(que)):
+                    if nj-1>0 and que[point][0] == ni and que[point][1] == nj-1:
+                        trigger = True
+                        break
+            if trigger:
+                trigger = False
+                break
+            if 0<=ni<N and 0<=nj<N and visit[ni][nj]==0:
+                if aqua[ni][nj] <= big:
+                    if aqua[ni][nj] == 0 or aqua[ni][nj] == big:
+                        visit[ni][nj]=1
+                        que.append([ni,nj,d+1,0,k])
+                    elif 0<aqua[ni][nj]<big:
+                        visit[ni][nj]=1
+                        que.append([ni,nj,d+1,aqua[ni][nj],k])
 print(t)
