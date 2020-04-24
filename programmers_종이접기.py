65ba7b4709524394e73f817b48fc806d1d545539
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-04-24 13:51:46 +0900

    0423까지 스터디 후

--- programmers_종이접기.py
+++ programmers_종이접기.py
@@ -1,4 +1,4 @@
-def solution(n):
+def solution_1(n):
     answer = [[0],[0,0,1]]
     for i in range(2,n+1):
         b=[0]*len(answer[i-1])
@@ -10,4 +10,20 @@ def solution(n):
     return answer[n-1]
 
 for i in range(1,4):
-    print(solution(i))
+    print(solution_1(i))
+
+memo = [[0]]
+def solution_2(n):
+    if len(memo) < n:
+        for i in range(len(memo)-1,n-1):
+            x = memo[i][::-1]
+            for j in range(len(x)):
+                if x[j] == 1:
+                    x[j] = 0
+                else:
+                    x[j] = 1
+            memo.append(memo[i]+[0]+x)
+    return memo[n-1]
+
+for i in range(1,4):
+    print(solution_2(i))
