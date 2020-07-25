44deb52f68acf8d1c328d3a372ec48683bbd7c90
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-07-25 23:58:19 +0900

    이걸 시간안에 못푸네....

---
+++ baekjoon_17182_우주탐사선.py
@@ -0,0 +1,31 @@
+answer = 0
+
+def solution(N,whe,cost=0):
+    global answer
+    if cost>=answer:
+        return
+    
+    if 0 not in visit:
+        if answer>cost:
+            answer = cost
+        return
+    
+    for i in range(N):
+        if i != whe and visit[i]<N-1:
+            visit[i] += 1
+            solution(N,i,cost+planet[whe][i])
+            visit[i] -= 1
+
+N,start = map(int,input().split())
+planet = [0]*N
+for i in range(N):
+    minimum = 1001
+    planet[i] = list(map(int,input().split()))
+    for j in range(N):
+        if planet[i][j] != 0 and minimum>planet[i][j]:
+            minimum = planet[i][j]
+    answer += minimum*N
+visit = [0]*N
+visit[start] = 1
+solution(N,start)
+print(answer)
