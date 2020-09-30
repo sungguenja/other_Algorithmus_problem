b0d502b353620df447e0a69bf36074fe072caa46
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-09-30 23:09:04 +0900

    큐를 조금 더 배워야할 거 같다 책을 봐야하는데....

--- baekjoon_3070_좋은친구_que.py
+++ baekjoon_3070_좋은친구_que.py
@@ -1,11 +1,11 @@
 import sys
 from collections import deque
-N,K = map(int,sys.stdin.readline().split())
+N,K = map(int,sys.stdin.readline()[:-1].split())
 student = [0]*N
 word_que = [deque() for i in range(21)]
 answer = 0
 for rank in range(N):
-    now = len(sys.stdin.readline())
+    now = len(sys.stdin.readline()[:-1])
     student[rank] = now
     if rank>K:
         word_que[student[rank-K-1]].popleft()
