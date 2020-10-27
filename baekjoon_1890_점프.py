09b92cb28b6d66a7454cb676bc8f3cc022d6f67f
sungguenja <59605197+sungguenja@users.noreply.github.com>
2020-10-27 20:58:19 +0900

    면접끝났다

---
+++ baekjoon_1890_점프.py
@@ -0,0 +1,23 @@
+N = int(input())
+board = [list(map(int,input().split())) for i in range(N)]
+memo = [[-1]*N for i in range(N)]
+visit = [[0]*N for i in range(N)]
+di = [0,1]
+dj = [1,0]
+def memoization(i=0,j=0):
+    if i==N-1 and j==N-1:
+        return 1
+    if visit[i][j] == 1:
+        return memo[i][j]
+    visit[i][j]=1
+    memo[i][j]=0
+    for k in range(2):
+        ni = i+board[i][j]*di[k]
+        nj = j+board[i][j]*dj[k]
+        if 0<=ni<N and 0<=nj<N:
+            if visit[ni][nj]==0:
+                memo[i][j] += memoization(ni,nj)
+
+    return memo[i][j]
+
+print(memoization())
