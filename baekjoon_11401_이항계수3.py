aef7ff0e61f6b9c785e6d876108cb0bf9843c582
sungguenja <59605197+sungguenja@users.noreply.github.com>
2021-03-25 19:13:39 +0900

    merge

--- baekjoon_11401_이항계수3.py
+++ baekjoon_11401_이항계수3.py
@@ -1,14 +1,23 @@
 N,K = map(int,input().split())
 answer = 1
-if N-K<=K:
-    for i in range(N,K,-1):
-        answer *= i
-    for i in range(N-K,0,-1):
-        answer //= i
-else:
-    for i in range(N,N-K,-1):
-        answer *= i
-    for i in range(K,0,-1):
-        answer //= i
+p = 1000000007
+
+factorial = [0]*(N+1)
+factorial[1] = 1
+for i in range(2,N+1):
+    factorial[i] = i*factorial[i-1]%p
+
+answer = factorial[N]%p
+B = (factorial[N-K]*factorial[K])%p
+
+def multiple(a,b):
+    if b==0:
+        return 1
+    if b%2 == 1:
+        return (multiple(a,b//2)**2*a)%p
+    else:
+        return (multiple(a,b//2)**2)%p
+
+answer = (answer*(multiple(B,p-2)%p))%p
 
 print(answer)
