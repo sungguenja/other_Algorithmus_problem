e584569a341f84c7a0c6e83ba2122adcc707ebd7
sungguenja <sungguenja@gmail.com>
2021-05-07 23:21:07 +0900

    ready coding test we must solve tomorrow

--- programmers_순위검색_re.py
+++ programmers_순위검색_re.py
@@ -1,136 +1,95 @@
+import bisect
 def makeQuery(string):
     string = string.split()
     string = [string[0],string[2],string[4],string[6],string[7]]
     return string
 
 def binarySearch(arr,N):
-    start = 0
-    end = len(arr)
-    mid = (start + end) // 2
-    while start < end:
-        if arr[mid] == N:
-            print(N,'hi')
-            return end - mid
-        if arr[mid] < N:
-            start = mid + 1
-            mid = (start + end) // 2
-        elif arr[mid] > N:
-            end = mid - 1
-            mid = (start + end) // 2
-    print(arr,N,start,mid,end)
-    return end - mid
+    size = len(arr)
+    return size - bisect.bisect_left(arr,N,lo=0,hi=size)
 
 def solution(info, query):
     answer = []
-    people_list = {
-        'cpp':{
-            'backend':{
-                'junior':{
-                    'chicken':[],
-                    'pizza':[]
-                },
-                'senior':{
-                    'chicken':[],
-                    'pizza':[]
-                }
-            },
-            'frontend':{
-                'junior':{
-                    'chicken':[],
-                    'pizza':[]
-                },
-                'senior':{
-                    'chicken':[],
-                    'pizza':[]
-                }
-            }
-        },
-        'java':{
-            'backend':{
-                'junior':{
-                    'chicken':[],
-                    'pizza':[]
-                },
-                'senior':{
-                    'chicken':[],
-                    'pizza':[]
-                }
-            },
-            'frontend':{
-                'junior':{
-                    'chicken':[],
-                    'pizza':[]
-                },
-                'senior':{
-                    'chicken':[],
-                    'pizza':[]
-                }
-            }
-        },
-        'python':{
-            'backend':{
-                'junior':{
-                    'chicken':[],
-                    'pizza':[]
-                },
-                'senior':{
-                    'chicken':[],
-                    'pizza':[]
-                }
-            },
-            'frontend':{
-                'junior':{
-                    'chicken':[],
-                    'pizza':[]
-                },
-                'senior':{
-                    'chicken':[],
-                    'pizza':[]
-                }
-            }
-        }
-    }
+    people_list = {}
 
     for i in range(len(info)):
         people = info[i]
         lang,job,carrer,food,point = people.split()
-        people_list[lang][job][carrer][food].append(int(point))
-        people_list[lang][job][carrer][food].sort()
-    
+        if people_list.get(lang+job+carrer+food) == None:
+            people_list[lang+job+carrer+food] = [int(point)]
+        else:
+            people_list[lang+job+carrer+food].append(int(point))
+        if people_list.get('-'+job+carrer+food) == None:
+            people_list['-'+job+carrer+food] = [int(point)]
+        else:
+            people_list['-'+job+carrer+food].append(int(point))
+        if people_list.get(lang+'-'+carrer+food) == None:
+            people_list[lang+'-'+carrer+food] = [int(point)]
+        else:
+            people_list[lang+'-'+carrer+food].append(int(point))
+        if people_list.get(lang+job+'-'+food) == None:
+            people_list[lang+job+'-'+food] = [int(point)]
+        else:
+            people_list[lang+job+'-'+food].append(int(point))
+        if people_list.get(lang+job+carrer+'-') == None:
+            people_list[lang+job+carrer+'-'] = [int(point)]
+        else:
+            people_list[lang+job+carrer+'-'].append(int(point))
+        if people_list.get('--'+carrer+food) == None:
+            people_list['--'+carrer+food] = [int(point)]
+        else:
+            people_list['--'+carrer+food].append(int(point))
+        if people_list.get('-'+job+'-'+food) == None:
+            people_list['-'+job+'-'+food] = [int(point)]
+        else:
+            people_list['-'+job+'-'+food].append(int(point))
+        if people_list.get('-'+job+carrer+'-') == None:
+            people_list['-'+job+carrer+'-'] = [int(point)]
+        else:
+            people_list['-'+job+carrer+'-'].append(int(point))
+        if people_list.get(lang+'--'+food) == None:
+            people_list[lang+'--'+food] = [int(point)]
+        else:
+            people_list[lang+'--'+food].append(int(point))
+        if people_list.get(lang+'-'+carrer+'-') == None:
+            people_list[lang+'-'+carrer+'-'] = [int(point)]
+        else:
+            people_list[lang+'-'+carrer+'-'].append(int(point))
+        if people_list.get(lang+job+'--') == None:
+            people_list[lang+job+'--'] = [int(point)]
+        else:
+            people_list[lang+job+'--'].append(int(point))
+        if people_list.get('---'+food) == None:
+            people_list['---'+food] = [int(point)]
+        else:
+            people_list['---'+food].append(int(point))
+        if people_list.get('--'+carrer+'-') == None:
+            people_list['--'+carrer+'-'] = [int(point)]
+        else:
+            people_list['--'+carrer+'-'].append(int(point))
+        if people_list.get('-'+job+'--') == None:
+            people_list['-'+job+'--'] = [int(point)]
+        else:
+            people_list['-'+job+'--'].append(int(point))
+        if people_list.get(lang+'---') == None:
+            people_list[lang+'---'] = [int(point)]
+        else:
+            people_list[lang+'---'].append(int(point))
+        if people_list.get('----') == None:
+            people_list['----'] = [int(point)]
+        else:
+            people_list['----'].append(int(point))
+        
     for i in range(len(query)):
         now_query = query[i]
         lang,job,carrer,food,point = makeQuery(now_query)
         cnt = 0
-        if lang == '-':
-            lang_list = ['cpp','java','python']
+        if point == '-':
+            cnt += len(people_list[lang+job+carrer+food])
         else:
-            lang_list = [lang]
-        
-        if job == '-':
-            job_list = ['frontend','backend']
-        else:
-            job_list = [job]
-        
-        if carrer == '-':
-            carrer_list = ['junior','senior']
-        else:
-            carrer_list = [carrer]
-        
-        if food == '-':
-            food_list = ['chicken','pizza']
-        else:
-            food_list = [food]
-        
-        for la in lang_list:
-            for jo in job_list:
-                for ca in carrer_list:
-                    for fo in food_list:
-                        if point == '-':
-                            cnt += len(people_list[la][jo][ca][fo])
-                            continue
-                        point = int(point)
-                        cnt += binarySearch(people_list[la][jo][ca][fo],point)
-        print(cnt)
+            point = int(point)
+            if people_list.get(lang+job+carrer+food) != None:
+                cnt += binarySearch(sorted(people_list.get(lang+job+carrer+food)), point)
         answer.append(cnt)
 
     return answer
