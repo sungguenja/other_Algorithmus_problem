92d23a2e81c9b8f76f28daad86d1823faad06244
sungguenja <sungguenja@gmail.com>
2021-07-12 13:26:04 +0900

    study

--- baekjoon_5022_연결bfs.py
+++ baekjoon_5022_연결bfs.py
@@ -12,29 +12,34 @@ def firstBfs(start,end,next_start,next_goal,N,M):
     answer = (N*M+1)**2
     after_answer = (N*M+1)**2
     visit = [[False]*(M+1) for _ in range(N+1)]
+    visit[start[0]][start[1]] = True
     Que.append((start[0],start[1],0,[(start[0],start[1])]))
     while Que:
         i,j,cnt,route = Que.popleft()
 
         if i == end[0] and j == end[1]:
-            answer = cnt
-            print(answer)
-            after_answer = min(after_answer,secondBfs(next_start,next_goal,N,M,route))
+            tmp_answer = cnt
+            tmp_after_answer = secondBfs(next_start,next_goal,N,M,route)
+            if tmp_answer + tmp_after_answer < answer + after_answer:
+                after_answer = tmp_after_answer
+                answer = tmp_answer
         
-        if after_answer != (N*M+1)**2 and answer != (N*M+1)**2:
-            return after_answer + answer
+        if cnt > after_answer + answer:
+            continue
 
         for k in range(4):
             ni = i + di[k]
             nj = j + dj[k]
-            if 0<=ni<=N and 0<=nj<=M and not visit[ni][nj] and (ni,nj) != next_start and (ni,nj) != next_goal:
+            if 0<=ni<=N and 0<=nj<=M and not visit[ni][nj] and not ((ni == next_start[0] and nj == next_start[1]) or (ni == next_goal[0] and nj == next_goal[1])):
                 visit[ni][nj] = True
                 Que.append((ni,nj,cnt+1,route+[(ni,nj)]))
     
+    if after_answer != (N*M+1)**2 and answer != (N*M+1)**2:
+        return after_answer + answer
+
     return "IMPOSSIBLE"
 
 def secondBfs(start,end,N,M,cant):
-    print(cant)
     di = [0,1,0,-1]
     dj = [1,0,-1,0]
     Que = deque()
@@ -59,7 +64,14 @@ def secondBfs(start,end,N,M,cant):
             if 0<=ni<=N and 0<=nj<=M and not visit[ni][nj] and (ni,nj) not in cant:
                 visit[ni][nj] = True
                 Que.append((ni,nj,cnt+1))
-    print(answer)
     return answer
 
-print(firstBfs(A1,A2,B1,B2,N,M))
+tmp_answer_1 = firstBfs(A1,A2,B1,B2,N,M)
+tmp_answer_2 = firstBfs(B1,B2,A1,A2,N,M)
+if tmp_answer_1 != "IMPOSSIBLE" and tmp_answer_2 != "IMPOSSIBLE":
+    print(min(tmp_answer_1,tmp_answer_2))
+else:
+    if tmp_answer_1 == "IMPOSSIBLE":
+        print(tmp_answer_2)
+    else:
+        print(tmp_answer_1)
