dd586e83370f5bf18750fbd584fe491d1eace48e
sungguenja <sungguenja@gmail.com>
2022-07-31 16:41:12 +0900

    study

--- programmers_카펫.py
+++ programmers_카펫.py
@@ -2,6 +2,7 @@ def solution(brown, yellow):
     core_brown = (brown - 4)//2
     for i in range(1,core_brown):
         j = core_brown - i
-        print(j)
-    answer = []
-    return answer
+        if i > j:
+            break
+        if i * j == yellow:
+            return [j+2,i+2]
